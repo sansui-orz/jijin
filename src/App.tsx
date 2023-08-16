e09e71f879bb880a6fb4bf975de9eb45ff520bfa
xueren <xueren@nukaka.com>
2023-08-16 09:59:13 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { useEffect, useState, useCallback, useRef } from 'react';
 import './App.css';
-import Item from './components/item'
+import Item, { IData } from './components/item'
 
-const ids = [
+const ids: Array<{
+  name: string;
+  code: string;
+  data?: IData
+}> = [
   {
     "name": "长信低碳环保行业量化股票A",
     "code": "004925"
@@ -42,16 +47,69 @@ const ids = [
     "name": "前海开源中航军工指数C",
     "code": "015046"
   },
-  {
-    "name": "招商中证煤炭等权指数(LOF)C",
-    "code": "013596"
-  }
+  // {
+  //   "name": "招商中证煤炭等权指数(LOF)C",
+  //   "code": "013596"
+  // }
 ]
 
+function jsonP(id: string) {
+  return new Promise<IData>((resolve, reject) => {
+    const script = document.createElement('script')
+    // @ts-ignore
+    window.jsonpgz = (json: IData) => {
+      document.body.removeChild(script)
+      // @ts-ignore
+      window.jsonpgz = () => {}
+      resolve(json)
+    }
+    script.src = `http://fundgz.1234567.com.cn/js/${id}.js?rt=1463558676006`
+    script.onerror = function() {
+      reject('加载基金数据错误')
+    }
+    document.body.appendChild(script)
+  })
+}
+
+async function fetchAll() {
+  for (let i = 0; i < ids.length; i++) {
+    console.log('name', ids[i].name)
+    ids[i].data = await jsonP(ids[i].code)
+    console.log('name done', ids[i].name)
+  }
+  return ids
+}
+
 function App() {
+  const [list, setList] = useState(ids)
+  useEffect(() => {
+    fetchAll().then((res) => {
+      setList([ ...res ])
+    })
+  }, [])
+  const inputRef = useRef<HTMLInputElement>(null)
+  const addHandle = useCallback(async () => {
+    try {
+      const text = inputRef.current?.value
+      const json = await jsonP(text!)
+      if (json.name && !list.find(item => item.code === text)) {
+        setList([...list, {
+          name: json.name,
+          code: text!,
+          data: json
+        }])
+      }
+    } catch {
+      window.alert('没有找到该基金')
+    }
+  }, [list])
   return (
     <div className="App">
-      { ids.map(item => (<Item key={item.code} dataId={item.code} name={item.name} />))}
+      <div>
+        <input ref={inputRef} placeholder="请输入六位基金代码"/>
+        <button onClick={addHandle}>添加</button>
+      </div>
+      { list.map(item => (<Item key={item.code} dataId={item.code} name={item.name} data={item.data} />))}
     </div>
   );
 }
--- src/components/item/index.tsx
+++ src/components/item/index.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './index.css'
 
-interface IData {
+export interface IData {
   fundcode: number;
   name: string;
   jzrq: string;
@@ -13,25 +13,13 @@ interface IData {
 
 const upOrDown = (count: string | number) => +count > 0 ? 'up' : 'down'
 
-function Item(props: { dataId: string, name: string }) {
-  const [data, setDate] = useState<IData | undefined>(undefined)
-  useEffect(() => {
-    async function getData() {
-      const res = await window.fetch(`/api/js/${props.dataId}.js?rt=1463558676006`)
-      const read = res.body?.getReader().read()
-      const { value } = await read!
-      const text = new TextDecoder().decode(value!);
-      const json = text.replace(/(jsonpgz\()|(\);)/g, '')
-      setDate(JSON.parse(json))
-    }
-    getData();
-  }, [])
+function Item(props: { dataId: string, name: string, data?: IData }) {
   return <div className="item">
-    {data ? <div className={upOrDown(data.gszzl)}>
-      <span className="name"><span>{data.name}</span></span>
-      <span className="jz">净值: <span>{data.dwjz}</span></span>
-      <span className="gsjz">估算净值: <span>{data.gsz}</span></span>
-      <span className="gszdf">估算涨跌幅: <span>{data.gszzl}%</span></span>
+    {props.data ? <div className={upOrDown(props.data.gszzl)}>
+      <span className="name"><span>{props.data.name}</span></span>
+      <span className="jz">净值: <span>{props.data.dwjz}</span></span>
+      <span className="gsjz">估算净值: <span>{props.data.gsz}</span></span>
+      <span className="gszdf">估算涨跌幅: <span>{props.data.gszzl}%</span></span>
     </div> : 
     (<div className="fail-msg">{props.name}请求失败</div>)}
   </div>
